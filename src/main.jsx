06a644c5cf03fd704b0457109e8232d653985a8b
facusteckler86 <facundomsteckler@gmail.com>
2025-02-08 01:28:02 -0300

    reviso errores de las carpetas

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,7 @@
 import React, { StrictMode } from 'react';
-//import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {createRoot} from 'react-dom/client';
-
-// eslint-disable-next-line react/no-deprecated
-
-//Bloqueo esta parte del codigo ya que me larga error al subrilo por consola
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
@@ -20,7 +9,7 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <App />
-    </StrictMode>
-)
+  </StrictMode>
+);
 
 
